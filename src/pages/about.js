ca144f1f64f0fca258c2037ae7dec6e40f051881
Plamen <plpetkov@tutanota.com>
2025-05-14 09:23:33 +0300

    Changing a few things

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const About = () => {
   return (
     <Layout>
       <Seo
-        title="About - Holo"
+        title="About - Mila | UI UX Designer"
         description="Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS."
       ></Seo>
       <AboutHeader></AboutHeader>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ const IndexPage = () => (
   <div className="h-auto w-screen">
     <Layout>
       <Seo
-        title="Holo - Gatsby Theme"
-        description="Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS."
+        title="Home - Mila | UI UX Designer"
+        description="Mila is a UI UX Designer based in Bulgaria. Her portfolio features Figma designs, fine arts and graphic design deliveries"
       ></Seo>
       <Header></Header>
       <FeatureSection></FeatureSection>
